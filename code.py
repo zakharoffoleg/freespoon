04248209efbe58057c45000814ebad03f2f0bfcb
Oleg Zakharov <o.zakharoff@gmail.com>
2019-03-12 20:23:35 +0300

    Created 1 function for all the requests: to bidded projects, to projects in work and to past projects.

--- code.py
+++ code.py
@@ -1,35 +1,44 @@
 import requests, json, bottle
+from freelancersdk.session import Session
+from freelancersdk.resources.users.users import get_self, get_portfolios
 
-token = 'md0o5M70M7nWNPP2DpMLk9yQlV8gYv'
-user_id = '33547025'
 
 #token = input("Enter token: ")
 #user_id = input("Enter user ID: ")
 
-#Get user_id info
-#url = "https://www.freelancer.com/api/users/0.1/users/33547025"
+token = 'md0o5M70M7nWNPP2DpMLk9yQlV8gYv'
+user_id = '33547025' #18921481
+session = Session(oauth_token=token)
 
+#Get user_id info
+#print(get_self(session,))
 
 #Get all bidded projects
+#print(get_portfolios(session, user_id))
 
-url = "https://www.freelancer.com/api/projects/0.1/self/?status=active&role=freelancer"
 
-headers = {'freelancer-oauth-v1': token}
+def get_projects(project_type):  # project_type = awarded/active/past
 
-response = requests.request("GET", url, headers=headers)
+    url = "https://www.freelancer.com/api/projects/0.1/self/?status="+project_type+"&role=freelancer"
+    headers = {'freelancer-oauth-v1': token}
+    response = requests.request("GET", url, headers=headers)
+    json_response = json.loads(response.text)
 
-json_response = json.loads(response.text)
+    #print(json_response)
+    n = 1
+    projects = ""
+    for i in json_response['result']['projects']['projects']:
+        projects += ("Project №" + str(n) + " - " + str(i['title']) + '<br>')
+        projects += ("Project id: " + str(i['id']) + '<br>')
+        projects += ("Project owner id: " + str(i['owner_id']) + '<br>')
+        projects += ("Project description: " + '\n' + str(i['description']).replace('\n', '<br>') + '<br><br>')
 
-n = 1
-projects = ""
-for i in json_response['result']['projects']['projects']:
-    projects += ("Project №" + str(n) + " - " + str(i['title']) + '<br>')
-    projects += ("Project id: " + str(i['id']) + '<br>')
-    projects += ("Project owner id: " + str(i['owner_id']) + '<br>')
-    projects += ("Project description: " + '\n' + str(i['description']).replace('\n', '<br>') + '<br><br>')
+    return projects
+
 
 @bottle.route('/')
 def index():
-    return bottle.template('<b>' + projects + '</b>')
+    return bottle.template('<b>' + get_projects("awarded") + '</b>')
+
 
 bottle.run(host='localhost', port=8080)
