f14e8040fcc3f34e6db66b3b74a16dcf730f5f0c
Oleg Zakharov <o.zakharoff@gmail.com>
2019-03-18 17:36:54 +0300

    Added unique id column, now checking if user with such id exists in database User ID is now primary key

--- toggl.py
+++ toggl.py
@@ -1,4 +1,4 @@
-import requests, psycopg2, json
+import requests, psycopg2, json, uuid
 from freelancersdk.session import Session
 from freelancersdk.resources.users.users import get_self
 from collections import defaultdict
@@ -11,9 +11,11 @@ fl_token = 'md0o5M70M7nWNPP2DpMLk9yQlV8gYv'
 conn = psycopg2.connect(dbname='postgres', user='postgres', password='admin', host='localhost', port='5432')
 cursor = conn.cursor()
 
+'''Добавить тест существующих юзеров + primary key с уникальными id'''
+
 
 class User:
-    def __init__(self, toggl_token, fl_token):  # add freelancer_token later
+    def __init__(self, toggl_token, fl_token):
         self.toggl_token = toggl_token
         self.fl_token = fl_token
 
@@ -28,14 +30,36 @@ class User:
 
     def get_fl_user(self):
         session = Session(oauth_token=self.fl_token)
-        self.fl_id, self.fl_email = get_self(session)['id'], get_self(session)['email']
+        self.fl_id, self.fl_email = str(get_self(session)['id']), get_self(session)['email']
 
         print(self.fl_id, self.fl_email)
 
-    def post_user_info(self):
-        cursor.execute('''INSERT INTO users (toggl_token, toggl_id, toggl_email, fl_token, fl_id, fl_email) VALUES 
-            (%s, %s, %s, %s, %s, %s)''',
-                       (self.toggl_token, self.toggl_id, self.toggl_email, self.fl_token, self.fl_id, self.fl_email))
+    def get_user_id(self):
+        try:
+            cursor.execute(
+                "SELECT id FROM users WHERE toggl_id = %s OR fl_id = %s OR toggl_token = %s OR fl_token = %s;",
+                (self.toggl_id, self.fl_id, self.toggl_token, self.fl_token))
+
+            if cursor.rowcount == 1:
+                for i in cursor:
+                    self.id = str(i[0])
+                    print(self.id)
+                    return self.id
+            else:
+                return "More than 1 unique value in table"
+        except psycopg2.Error as e:
+            print(e)
+            pass
+
+    def register_user(self):
+        try:
+            cursor.execute('''INSERT INTO users (toggl_token, toggl_id, toggl_email, fl_token, fl_id, fl_email, id) 
+            VALUES (%s, %s, %s, %s, %s, %s, uuid_generate_v4())''',
+                           (
+                           self.toggl_token, self.toggl_id, self.toggl_email, self.fl_token, self.fl_id, self.fl_email))
+        except psycopg2.Error as e:
+            print(e)
+            pass
 
     def get_toggl_clients(self):
         url = "https://www.toggl.com/api/v8/clients"
@@ -60,15 +84,20 @@ class User:
                 self.fl_clients[project_type].append(str(client['owner_id']))
 
     def post_user_clients(self):
-        cursor.execute('''INSERT INTO users (toggl_clients, fl_clients) VALUES (%s, %s)''',
-                       (json.dumps(self.toggl_clients), json.dumps(self.fl_clients)))
+        try:
+            cursor.execute("UPDATE users SET toggl_clients = %s, fl_clients = %s WHERE id = %s;",
+                           (json.dumps(self.toggl_clients), json.dumps(self.fl_clients), self.id))
+        except psycopg2.Error as e:
+            print(e)
+            pass
 
 
 Oleg = User(toggl_token, fl_token)
-#Oleg.get_toggle_user()
-#Oleg.get_fl_user()
+Oleg.get_toggle_user()
+Oleg.get_fl_user()
+#Oleg.register_user()
+Oleg.get_user_id()
 Oleg.get_toggl_clients()
 Oleg.get_fl_clients()
 Oleg.post_user_clients()
-#Oleg.post_user_info()
 conn.commit()
